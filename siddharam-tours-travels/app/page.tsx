d5b149961ef6effe274fc8a20d6741442eb486d2
Nirmal kadam <119092476+Nirmalkadam@users.noreply.github.com>
2024-12-09 11:11:29 +0000

    Done

--- siddharam-tours-travels/app/page.tsx
+++ siddharam-tours-travels/app/page.tsx
@@ -4,10 +4,12 @@ import { FeaturedCars } from '@/components/featured-cars'
 import { OurTeam } from '@/components/our-team'
 import { Services } from '@/components/services'
 import { Footer } from '@/components/footer'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
+      <Analytics/>
       <Header />
       <main className="flex-grow">
         <Hero />
